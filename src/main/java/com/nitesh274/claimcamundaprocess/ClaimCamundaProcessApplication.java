bb78806a3446a628d315b104cae1f82275ba9f64
Nitesh Thakur <nthaku36@in.ibm.com>
2020-05-20 16:46:06 +0530

    first commit

---
+++ src/main/java/com/nitesh274/claimcamundaprocess/ClaimCamundaProcessApplication.java
@@ -0,0 +1,13 @@
+package com.nitesh274.claimcamundaprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimCamundaProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimCamundaProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitesh274/claimcamundaprocess/ClaimCamundaProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitesh274.claimcamundaprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimCamundaProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
